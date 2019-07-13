8bc2e30657d2e22717fa31618592b9e034749c73
Gustavo Enndi <gustavo_enndi@hotmail.com>
2019-07-13 18:04:51 -0300

    User password, UserDTO e Bean Bcrypt

--- src/main/java/com/enndi/authentication/model/User.java
+++ src/main/java/com/enndi/authentication/model/User.java
@@ -3,10 +3,13 @@
  */
 package com.enndi.authentication.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
  * @author gustavo
@@ -18,4 +21,63 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	private String name;
+
+	@Column(unique = true)
+	private String email;
+
+	@JsonIgnore
+	private String password;
+
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param id
+	 * @param name
+	 * @param email
+	 * @param password
+	 */
+	public User(Integer id, String name, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
