f8ba274e07b92d5be75a074babc5d89815bde488
Gustavo Enndi <gustavo_enndi@hotmail.com>
2019-07-20 15:44:35 -0300

    Swagger config

--- src/main/java/com/enndi/authentication/AuthenticationApplication.java
+++ src/main/java/com/enndi/authentication/AuthenticationApplication.java
@@ -2,20 +2,11 @@ package com.enndi.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class AuthenticationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationApplication.class, args);
 	}
-	
-    @RequestMapping("/")
-    public String home() {
-        return "Hello Docker World";
-    }
-
 }
--- src/main/java/com/enndi/authentication/config/SecurityConfig.java
+++ src/main/java/com/enndi/authentication/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -53,8 +54,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	public static final String[] PUBLIC_MATCHERS_POST = { "/auth/forgot/**" };
 
-	
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
@@ -66,9 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll()
 				.anyRequest().authenticated();
 
-		http.logout()
-			.invalidateHttpSession(true)
-			.logoutSuccessUrl("/login");
+		http.logout().invalidateHttpSession(true).logoutSuccessUrl("/login");
 
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
@@ -92,6 +89,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 		super.configure(auth);
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**",
+				"/swagger-ui.html", "/webjars/**");
 	}
 
 }
