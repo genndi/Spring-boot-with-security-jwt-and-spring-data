5721dcc70b643ee99954fa51c029d5892d2317c7
Gustavo Enndi <gustavo_enndi@hotmail.com>
2019-07-20 15:38:08 -0300

    create and enable swagger config

---
+++ src/main/java/com/enndi/authentication/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.enndi.authentication.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author gustavo
+ *
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+	}
+}
